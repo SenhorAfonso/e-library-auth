befe68a2bee328f1f35f6510b3443cb1ed2fb12c
Pedro Afonso <sirafonso005896@outlook.com>
2024-12-16 15:35:39 -0300

    :sparkles: implement the usecases in controller

--- src/main/java/com/sirafonso/e_library_users/adapters/in/web/controllers/AuthController.java
+++ src/main/java/com/sirafonso/e_library_users/adapters/in/web/controllers/AuthController.java
@@ -1,6 +1,13 @@
 package com.sirafonso.e_library_users.adapters.in.web.controllers;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
+import com.sirafonso.e_library_users.adapters.in.web.controllers.dtos.request.LoginUserRequest;
+import com.sirafonso.e_library_users.adapters.in.web.controllers.dtos.request.RegisterUserRequest;
+import com.sirafonso.e_library_users.core.ports.in.LoginUserInputPort;
+import com.sirafonso.e_library_users.core.ports.in.RegisterUserInputPort;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("auth")
 public class AuthController {
 
+    private final RegisterUserInputPort registerUserUsecase;
+    private final LoginUserInputPort loginUserInputPort;
+
+    public AuthController(
+        RegisterUserInputPort registerUserUsecase,
+        LoginUserInputPort loginUserInputPort
+    ) {
+        this.registerUserUsecase = registerUserUsecase;
+        this.loginUserInputPort = loginUserInputPort;
+    }
+
     @PostMapping("/register")
-    public String registerUser() {
-        return "registering user";
+    public String registerUser(@RequestBody @Valid RegisterUserRequest registerUserRequest) {
+        System.out.println(registerUserRequest);
+        return this.registerUserUsecase.execute(registerUserRequest.toUserModelIn());
     }
 
     @PostMapping("/login")
-    public String loginUser() {
-        return "login user";
+    public String loginUser(@RequestBody LoginUserRequest loginUserRequest) {
+        return this.loginUserInputPort.execute(loginUserRequest.email(), loginUserRequest.password());
     }
 
 }
